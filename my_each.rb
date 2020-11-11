5f19ff374767861900d9049e600a8fd8e24c884e
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 22:12:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,10 @@
 def my_each (array) # put argument(s) here
   # code here
   count = 0
-
+  arr = []
   while count < array.length
     yield array[count]
+    arr.push(array[count])
     count += 1
   end
 end
